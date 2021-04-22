6bde1c06e6ee7c0c20dc6b0bc6bdfff248e46bc0
root <root@DESKTOP-CROKRUC.localdomain>
2021-04-22 20:50:18 +0200

    Update

--- src/app/component/form-experience/form-experience.component.spec.ts
+++ src/app/component/form-experience/form-experience.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FormExperienceComponent } from './form-experience.component';
 
@@ -6,7 +6,7 @@ describe('FormExperienceComponent', () => {
   let component: FormExperienceComponent;
   let fixture: ComponentFixture<FormExperienceComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FormExperienceComponent ]
     })
--- src/app/component/infos/infos.component.spec.ts
+++ src/app/component/infos/infos.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InfosComponent } from './infos.component';
 
@@ -6,7 +6,7 @@ describe('InfosComponent', () => {
   let component: InfosComponent;
   let fixture: ComponentFixture<InfosComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InfosComponent ]
     })
